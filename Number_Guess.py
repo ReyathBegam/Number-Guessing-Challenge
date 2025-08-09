978eef73ae39f476a3823df1d01da763025ac3de
ReyathBegam <reyath.begam@gmail.com>
2025-08-09 13:04:44 +0530

    done

---
+++ Number_Guess.py
@@ -0,0 +1,16 @@
+import random
+number_to_guess=random.randint(1,100)
+guess=None
+attempt=0
+print("Welcome to number guessing challenge!!")
+print("I am thinking of a number from 1 to 100")
+while guess!=number_to_guess:
+    guess=int(input("Enter your guess:"))
+    attempt+=1
+    if guess<number_to_guess:
+        print("Too low..,Try again")
+    elif guess>number_to_guess:
+        print("Too high..,Try again")
+    else:
+        print(f"Congratulation!!!!, You guessed the number on {attempt} tr{'y'if attempt==1 else 'ies'}.")
+    
