7552da872c773cc8744ca36a3d55bce02ec504a4
ReyathBegam <reyath.begam@gmail.com>
2025-08-10 11:12:24 +0530

    changed

--- Number_Guess.py
+++ Number_Guess.py
@@ -5,12 +5,14 @@ attempt=0
 print("Welcome to number guessing challenge!!")
 print("I am thinking of a number from 1 to 100")
 while guess!=number_to_guess:
-    guess=int(input("Enter your guess:"))
-    attempt+=1
-    if guess<number_to_guess:
-        print("Too low..,Try again")
-    elif guess>number_to_guess:
-        print("Too high..,Try again")
-    else:
-        print(f"Congratulation!!!!, You guessed the number on {attempt} tr{'y'if attempt==1 else 'ies'}.")
-    
+    try:
+        guess=int(input("Enter your guess:"))
+        attempt+=1
+        if guess<number_to_guess:
+            print("Too low..,Try again")
+        elif guess>number_to_guess:
+            print("Too high..,Try again")
+        else:
+            print(f"Congratulation!!!!, You guessed the number on {attempt} tr{'y'if attempt==1 else 'ies'}.")
+    except valueError:
+        print("Invalid input. Please enter a valid number")
